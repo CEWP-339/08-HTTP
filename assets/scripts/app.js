99988fabb2d8d9249ff8a39f04641fe38164c4a3
Kritika Sharma <kritikasharma@Kritikas-MacBook-Pro.local>
2022-08-22 19:24:00 -0400

    post request + restructure code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,43 @@
-const xhr = new XMLHttpRequest();
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+function sendHTTPRequest(method, url, data) {
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.responseType = 'json';
+        xhr.onload = function() {
+            resolve(xhr.response);
+        }
+        xhr.send(JSON.stringify(data));
+    });
+    return promise;
+}
 
-console.log(xhr.response); //this will not work.
-
-xhr.responseType = 'json';
-
-xhr.onload = function() {
-    // const listOfPosts = JSON.parse(xhr.response);
-    const listOfPosts = xhr.response;
+async function fetchPosts() {
+    const responseData = await sendHTTPRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
+    const listOfPosts = responseData;
     for (const post of listOfPosts) {
-        console.log(post);
+        const postElement = document.importNode(postTemplate.content, true);
+        postElement.querySelector('h2').textContent = post.title.toUpperCase();
+        postElement.querySelector('p').textContent = post.body;
+        listElement.append(postElement);
     }
 }
 
-xhr.send();
+async function createPost(title, content) {
+    const userId = Math.random();
+    const post = {
+        title,
+        body: content,
+        userId
+    }
+    sendHTTPRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
+}
+
+fetchPosts();
+createPost("DUMMY", "This is a dummy post");
+
+
+
+
