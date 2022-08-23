08c76cc1498fd1664dd1771eef61e6b542a0fa42
Kritika Sharma <kritikasharma@Kritikas-MacBook-Pro.local>
2022-08-22 20:36:34 -0400

    adding headers

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,7 +6,8 @@ const postList = document.querySelector("ul");
 
 function sendHTTPRequest(method, url, data) {
 //   const promise = new Promise((resolve, reject) => {
-//     const xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
+    xhr.setRequestHeader('Content-Type', 'application/json');
 //     xhr.open(method, url);
 //     xhr.responseType = "json";
 //     xhr.onload = function () {
@@ -25,7 +26,10 @@ function sendHTTPRequest(method, url, data) {
 
     return fetch(url, {
         method,
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
     }).then(response => {
         return response.json();
     });
